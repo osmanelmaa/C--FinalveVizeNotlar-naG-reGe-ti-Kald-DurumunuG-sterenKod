ad7d7704c1d242b037e14be1e7aea1c26577c4dc
Osman Ali Elma <128042882+osmanelmaa@users.noreply.github.com>
2023-11-15 21:36:05 +0300

    Add files via upload

---
+++ FinalveVizeNotlarıHesaplama/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System.ComponentModel;
+
+Console.WriteLine("Lütfen Vize Notunuzu Giriniz:");
+int vize = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen Final Notunuzu Giriniz:");
+int final = Convert.ToInt32(Console.ReadLine());
+
+
+double sonuc = (vize * 0.4) + (final * 0.6);
+Console.WriteLine("Ortalama {0}", sonuc);
+
+if (sonuc > 60)
+{
+    Console.WriteLine("Tebrikler Dersi Geçtiniz");
+}
+else
+{
+    Console.WriteLine("Maalesef Dersten Kaldınız");
+}
+
+
+
